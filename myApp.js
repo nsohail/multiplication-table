ef16df931436b3ad7a63d7a35ffad11b7e877a88
Neha Sohail <nsohail92@gmail.com>
2015-05-07 00:14:31 -0400

    v1 milestone

--- myApp.js
+++ myApp.js
@@ -1,4 +1,18 @@
 angular.module('myApp',[])
-	.controller('myCtrl',function($scope){
+	.controller('MultiplicationCtrl',function($scope, $attrs){
+		$scope.numberLimit = $attrs.initialNumberLimit || 10;
+
+		$scope.numbers = function() {
+			var numbers = [];
+			for (var i=0; i < $scope.numberLimit; i++) {
+				numbers[i] = i+1;
+			}
+			return numbers;
+		};
+
+		$scope.compute = function(a,b) {
+			return a * b;
+		};
+
 		
 	});
